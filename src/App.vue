a5817388ef7d59165c7ab151fc17f026991a6565
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-03-14 17:05:12 -0300

    Feat: Pimeros componentes y comuniacion entre ellos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+   <main>
+      <px-header></px-header>
+      <px-assets-table></px-assets-table>
+   </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from '@/components/PxHeader';
+import PxAssetsTable from "@/components/PxAssestsTable";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+   name: "App",
+   components: {PxHeader, PxAssetsTable}
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
 }
 </style>
