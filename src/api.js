6f4ad2e2c54c31514ee8c254f5f574acc1536a3c
oscar-soto <oscarantonio.soto1998@gmail.com>
2021-04-21 17:20:46 -0300

    Fix: add min, max and avg value

--- src/api.js
+++ src/api.js
@@ -14,7 +14,19 @@ function getAssest(coin) {
       
 }
 
+function getAssestHistory(coin) {
+   const now = new Date();
+   const end = now.getTime();
+   now.setDate(now.getDate() - 1);
+   const start = now.getTime();
+
+   return fetch(`${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`)
+      .then(res => res.json())
+      .then(sol => sol.data)
+}
+
 export default {
    getAssests,
-   getAssest
+   getAssest,
+   getAssestHistory
 }
--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -5,9 +5,11 @@
             <div class="flex flex-col items-center">
                <img 
                   :src="`https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`"
+                  :alt="asset.name"
                   class="w-20 h-20 mr-5" />
                <h1 class="text-5xl">
-                  <small class="sm:mr-2 text-gray-500"></small>
+                  {{asset.name}}
+                  <small class="sm:mr-2 text-gray-500">{{asset.symbol}}</small>
                </h1>
             </div>
 
@@ -15,33 +17,33 @@
                <ul>
                   <li class="flex justify-between">
                      <b class="text-gray-600 mr-10 uppercase">Ranking</b>
-                     <span></span>
+                     <span>#{{asset.rank}}</span>
                   </li>
                   <li class="flex justify-between">
                      <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
-                     <span></span>
+                     <span>{{asset.priceUsd | dollar}}</span>
                   </li>
                   <li class="flex justify-between">
                      <b class="text-gray-600 mr-10 uppercase"
                         >Precio más bajo</b
                      >
-                     <span></span>
+                     <span>{{min | dollar}}</span>
                   </li>
                   <li class="flex justify-between">
                      <b class="text-gray-600 mr-10 uppercase"
                         >Precio más alto</b
                      >
-                     <span></span>
+                     <span>{{max | dollar}}</span>
                   </li>
                   <li class="flex justify-between">
                      <b class="text-gray-600 mr-10 uppercase"
                         >Precio Promedio</b
                      >
-                     <span></span>
+                     <span>{{avg | dollar}}</span>
                   </li>
                   <li class="flex justify-between">
                      <b class="text-gray-600 mr-10 uppercase">Variación 24hs</b>
-                     <span></span>
+                     <span>{{asset.changePercent24Hr | percent}}</span>
                   </li>
                </ul>
             </div>
@@ -79,7 +81,24 @@
 
       data() {
          return {
-            asset:{}
+            asset:{},
+            history: []
+         }
+      },
+
+      computed: {
+         min() {
+            return Math.min(
+               ...this.history.map( h => parseFloat(h.priceUsd).toFixed(2))
+            )
+         },
+         max() {
+            return Math.max(
+               ...this.history.map( h => parseFloat(h.priceUsd).toFixed(2))
+            )
+         },
+         avg() {
+            return this.history.reduce((a,b) => a + parseFloat(b.priceUsd), 0) / this.history.length
          }
       },
 
@@ -90,8 +109,17 @@
       methods: {
          getCoin () {
             const id = this.$route.params.id;
-            api.getAssest(id)
-               .then(asset => this.asset = asset)
+
+            Promise.all([
+               api.getAssest(id),
+               api.getAssestHistory(id)
+            ])
+            .then( ([asset, history]) => {
+               this.asset = asset
+               this.history = history
+            })
+
+            
          }
       },
    }
